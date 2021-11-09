48971839997338ab31e6b584154725cebfb33b07
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-11-09 12:17:30 -0800

    package installations

---
+++ r-package-installation.R
@@ -0,0 +1,5 @@
+# GitHub-based packages commonly used
+
+devtools::install_github("PNNL-TES/picarro.data")
+devtools::install_github("miraKlein/ggbiplot")
+devtools::install_github("kaizadp/soilpalettes")
