639c5348dbcd57848bf044dde481c61ac60aaa97
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-02-13 10:45:26 -0800

    update

--- testing_action.R
+++ testing_action.R
@@ -1,5 +1,6 @@
 
-SHEET_NAME = "https://docs.google.com/spreadsheets/d/1yDE1m9J04ljDzzvKLqZIxL7xwMoIB1W3j5kpeOjUvoo/edit?gid=0#gid=0"
+## SHEET_NAME = "https://docs.google.com/spreadsheets/d/1yDE1m9J04ljDzzvKLqZIxL7xwMoIB1W3j5kpeOjUvoo/edit?gid=0#gid=0"
 
-x = read_sheet(SHEET_NAME)
+## x = read_sheet(SHEET_NAME)
+x = c(0,1,2)
 write.csv(x, "test.csv")
