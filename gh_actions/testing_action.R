784827aa756581c89e9d0a466cd9d88eca1886fc
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-02-13 10:07:08 -0800

    update packages

--- gh_actions/testing_action.R
+++ gh_actions/testing_action.R
@@ -1,10 +1,8 @@
 
 
-library(tidyverse)
-library(googledrive)
 library(googlesheets4)
 
 SHEET_NAME = "https://docs.google.com/spreadsheets/d/1yDE1m9J04ljDzzvKLqZIxL7xwMoIB1W3j5kpeOjUvoo/edit?gid=0#gid=0"
 
 x = read_sheet(SHEET_NAME)
-x %>% write.csv(SHEETNAME)
+write.csv(x, SHEETNAME)
