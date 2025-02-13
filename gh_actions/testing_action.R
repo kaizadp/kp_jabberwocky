c34c226e125cb118853980339845f5a66e56d281
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-02-13 10:33:54 -0800

    update

--- gh_actions/testing_action.R
+++ gh_actions/testing_action.R
@@ -2,4 +2,4 @@
 SHEET_NAME = "https://docs.google.com/spreadsheets/d/1yDE1m9J04ljDzzvKLqZIxL7xwMoIB1W3j5kpeOjUvoo/edit?gid=0#gid=0"
 
 x = read_sheet(SHEET_NAME)
-write.csv(x, path = "test.csv")
+write.csv(x, "test.csv")
